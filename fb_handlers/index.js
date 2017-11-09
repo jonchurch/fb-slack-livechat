a0c39493800a59e79f6c594f19422e9f269f5721
jonchurch <me@jonchurch.com>
2017-11-08 22:40:07 -0500

    stub out post to slack

--- fb_handlers/index.js
+++ fb_handlers/index.js
@@ -4,6 +4,14 @@ module.exports = (slack_controller, facebook_controller) => {
 
 	facebook_controller.on('standby', (bot, message) => {
 		console.log('Another standby message...')
+
+		facebook_controller.api.handover.take_thread_control(message.user, (err, res) => {
+			if (err) {
+				console.log(err)
+			} else {
+				console.log({res})
+			}
+		})
 	})
 
 	facebook_controller.on('facebook_receive_thread_control', (bot, message) => {
@@ -13,8 +21,22 @@ module.exports = (slack_controller, facebook_controller) => {
 		console.log('Saw that FB event!')
 
 		bot.reply(message, 'Heard your facebook message!')
+		postToSlack(message)
 
 		// const bot = slack_controller.spawn()
 
 	})
+
+	function postToSlack(message) {
+
+		// find the access_token for the Slack team associated with the FB page
+		const team = slack_controller.getTeamConfigForPage(message)
+
+		// spawn a bot that is configured to respond in slack context
+		const bot = slack_controller.spawn(team)
+
+		// I need to take this FB message, with the User info, and Post it into slack...
+		slack_controller.trigger('facebook_message', [bot, message])
+		
+	}
 }
