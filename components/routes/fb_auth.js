dfb4b6c8d04a8a0a90862c1143f51185dc72d604
jonchurch <me@jonchurch.com>
2017-11-14 22:18:25 -0500

    send selected page back to server to be saved in db

--- components/routes/fb_auth.js
+++ components/routes/fb_auth.js
@@ -4,11 +4,9 @@ module.exports = (webserver, slack_controller, facebook_controller) => {
 	webserver.get('/facebook/auth', (req, res) => {
 		rp('https://graph.facebook.com/v2.11/me/accounts?access_token=' + req.query.token)
 		.then(pages => {
-			// populate a dropdown to select page
-			console.log('pages:\n',pages)
-			
+
 			res.json(pages)
-		
+
 			}).catch(err => res.send(false))
 	})
 
@@ -16,22 +14,25 @@ module.exports = (webserver, slack_controller, facebook_controller) => {
 		// sets up a facebook page
 		const page = req.body.page
 		rp.post('https://graph.facebook.com/me/subscribed_apps?access_token=' + page.access_token)
-		.then(res => {
-			if (res.success) {
-				facebook_controller.debug.log(`Successfully subscribed to ${page.name}'s messages'`)
+		.then(result => {
+			if (result.success) {
+				facebook_controller.debug(`Successfully subscribed to ${page.name}'s messages'`)
 				facebook_controller.storage.teams.save({
 					id: page.id,
 					access_token: page.access_token,
 					name: page.name
 				}, (err, team) => {
 					if (err) {
-						facebook_controller.debug.log('Error saving team')
+						facebook_controller.debug('Error saving team')
 					} else {
-						facebook_controller.debug.log('Saved team', team)
+						facebook_controller.debug('Saved team', team)
+						res.json(result)
 					}
 				})
+			} else {
+				res.json(result)
 			}
-		})
+		}).catch(err => res.json(err.message))
 
 	})
 }
